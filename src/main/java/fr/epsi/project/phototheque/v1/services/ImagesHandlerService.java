8d854354f77c2ded7e93f5302882556e68078792
Eliseo Murillo <eliseomurillo9@gmail.com>
2024-03-20 07:56:24 +0100

    image handler

--- src/main/java/fr/epsi/project/phototheque/v1/services/ImagesHandlerService.java
+++ src/main/java/fr/epsi/project/phototheque/v1/services/ImagesHandlerService.java
@@ -1,7 +1,12 @@
 package fr.epsi.project.phototheque.v1.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
@@ -10,6 +15,9 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Base64;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Consumer;
 
 @Service
 public class ImagesHandlerService {
@@ -17,8 +25,45 @@ public class ImagesHandlerService {
     private String api_key;
 
     public void imageHandler(String urlString, MultipartFile imageFile) throws IOException {
-        URL url = new URL(urlString);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        String jsonBody = "{\"filename\": \"" + imageFile.getOriginalFilename() + "\"}";
+        var fileInfo = imageFile.getInputStream();
+
+
+        RestClient restClient = RestClient.create();
+        String result = restClient.post()
+                .uri(urlString)
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(httpHeaders -> {
+                    httpHeaders.set("x-api-key", api_key);
+                })
+                .body(jsonBody)
+                .retrieve()
+                .body(String.class);
+
+
+        if (result != null) {
+            System.out.println(result);
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, String> responseMap = mapper.readValue(result, Map.class);
+            ByteArrayResource resource = new ByteArrayResource(imageFile.getBytes());
+            System.out.println(resource);
+
+            // Access the signedUrl
+            String signedUrl = responseMap.get("signedUrl");
+
+            String imageAnalizer = restClient.put()
+                    .uri(signedUrl)
+                    .headers(httpHeaders -> {
+                        httpHeaders.set("Content-Type", "image/jpeg");
+                    })
+                    .body(resource)
+                    .retrieve()
+                    .body(String.class);
+
+
+            System.out.println(imageAnalizer);
+        }
+  /**      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("POST");
 
         String jsonBody = "{\"filename\": \"" + imageFile.getOriginalFilename() + "\"}";
@@ -38,7 +83,7 @@ public class ImagesHandlerService {
         } else {
             System.out.println("ERROR: " + responseCode);
         }
-        connection.disconnect();
+        connection.disconnect(); */
     }
 }
 
